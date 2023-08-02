bfb134068bcc5279c4b3c00f8963287facc44e75
Richard Chukwu <richinex@gmail.com>
2023-08-02 21:56:07 +0200

    Add Pre-integration Tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,4 +2,13 @@ node('workers'){
     stage('Checkout'){
         checkout scm
     }
+
+    def imageTest= docker.build("${imageName}-test", "-f Dockerfile.test .")
+    stage('Pre-integration Tests'){
+        parallel(
+            'Quality Tests': {
+                sh "docker run --rm ${imageName}-test npm run lint"
+            }
+        )
+    }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,17 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
-  { 
+  {
     path: 'dashboard',
     component: DashboardComponent,
     data: { title: 'Sign In - Komiser' }
   },
-  { 
+  {
     path: 'favorites',
     component: FavoritesComponent,
     data: { title: 'Join - Komiser' }
   },
-  { 
+  {
     path: 'movies/:id',
     component: MovieComponent,
     data: { title: 'Expired token - Komiser' }
