765c7ddfb352adb85ceda532464229cabe42b8e6
Richard Chukwu <richinex@gmail.com>
2023-08-03 09:47:23 +0200

    Add Ouality and Unit Tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,3 +1,5 @@
+/* tslint:disable */
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
 import { RouterTestingModule } from '@angular/router/testing'; // Import RouterTestingModule for routerLink
