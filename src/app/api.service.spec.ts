97ef15b853fbcef2700554c44d6f8fb4286fcb92
Richard Chukwu <richinex@gmail.com>
2023-08-03 07:42:42 +0200

    Add Ouality and Unit Tests

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,13 +1,15 @@
 /* tslint:disable */
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import this
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule] // Add this line
+  }));
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+    const service: ApiService = TestBed.inject(ApiService); // Use inject instead of get, as get is deprecated
     expect(service).toBeTruthy();
   });
 });
