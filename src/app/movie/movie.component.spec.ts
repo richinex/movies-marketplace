2275d8f618e569a0eade15c2e0cfd647bfb499e4
Richard Chukwu <richinex@gmail.com>
2023-08-03 17:19:42 +0200

    Add Ouality and Unit Tests

--- src/app/movie/movie.component.spec.ts
+++ src/app/movie/movie.component.spec.ts
@@ -43,48 +43,33 @@
 // });
 
 /* tslint:disable */
-/* tslint:disable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { MovieComponent } from './movie.component';
 import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
-  let fixture: ComponentFixture<MovieComponent>;
   let apiServiceMock: any;
-
-  beforeEach(async(() => {
-    apiServiceMock = {
-      getMovie: jasmine.createSpy('getMovie').and.returnValue({ subscribe: () => {} }),
-      addToFavorites: jasmine.createSpy('addToFavorites').and.returnValue({ subscribe: () => {} }),
-    };
-
-    TestBed.configureTestingModule({
-      declarations: [MovieComponent],
-      providers: [
-        { provide: ApiService, useValue: apiServiceMock }, // Provide the mock ApiService
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              params: { id: 'exampleId' },
-            },
-          },
-        }, // Mock the ActivatedRoute with a snapshot object
-      ],
-    }).compileComponents();
-
-    // Correctly mock jQuery with the notify function
-    global['$'] = jasmine.createSpy('$').and.returnValue({
-      notify: jasmine.createSpy('notify'),
-    });
-  }));
+  let activatedRouteMock: any;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MovieComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiServiceMock = jasmine.createSpyObj('ApiService', ['getMovie', 'addToFavorites']);
+    apiServiceMock.addToFavorites.and.returnValue(of({})); // Use of({}) to return an observable
+    apiServiceMock.getMovie.and.returnValue(of({}));
+
+    activatedRouteMock = {
+      snapshot: {
+        params: { id: 1 },
+      },
+    };
+
+    component = new MovieComponent(apiServiceMock, activatedRouteMock);
+
+    // Mocking $.notify
+    window['$'] = {
+      notify: jasmine.createSpy('notify')
+    };
   });
 
   it('should create', () => {
@@ -94,14 +79,10 @@ describe('MovieComponent', () => {
   it('should call addToFavorites', () => {
     const mockMovieData = { title: 'Test Movie' };
     component.movie = mockMovieData;
-    apiServiceMock.addToFavorites.and.returnValue({
-      subscribe: (callback: Function) => callback(),
-    });
 
     component.addToFav();
 
     expect(apiServiceMock.addToFavorites).toHaveBeenCalledWith(mockMovieData);
-    // Now this should work
-    expect(global['$']().notify).toHaveBeenCalled();
+    expect(window['$'].notify).toHaveBeenCalled();
   });
 });
