96087aaa73b2911b415ca06f4c8d4751896c2d58
Richard Chukwu <richinex@gmail.com>
2023-08-03 09:17:07 +0200

    Add Ouality and Unit Tests

--- src/app/favorites/favorites.component.spec.ts
+++ src/app/favorites/favorites.component.spec.ts
@@ -1,7 +1,8 @@
-/* tslint:disable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing'; // Import for router testing
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // If ApiService uses HttpClient
 import { FavoritesComponent } from './favorites.component';
+import { ApiService } from '../api.service'; // If ApiService is used
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
@@ -9,7 +10,9 @@ describe('FavoritesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FavoritesComponent ]
+      declarations: [ FavoritesComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule], // Add these lines for router and HttpClient testing
+      providers: [ApiService] // If ApiService is used
     })
     .compileComponents();
   }));
