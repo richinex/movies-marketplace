2936aea60c4e6a1cd20415727d3090fcef023b31
Richard Chukwu <richinex@gmail.com>
2023-08-03 09:56:01 +0200

    Add Ouality and Unit Tests

--- src/app/movie/movie.component.spec.ts
+++ src/app/movie/movie.component.spec.ts
@@ -1,18 +1,34 @@
 /* tslint:disable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import HttpClientTestingModule
+import { ActivatedRoute } from '@angular/router';
 import { MovieComponent } from './movie.component';
+import { ApiService } from '../api.service';
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
 
+  // Create a mock for the ApiService
+  const apiServiceMock = {
+    getMovie: jasmine.createSpy('getMovie').and.returnValue({ subscribe: () => {} }),
+    addToFavorites: jasmine.createSpy('addToFavorites').and.returnValue({ subscribe: () => {} }),
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovieComponent ],
-      imports: [HttpClientTestingModule] // Include HttpClientTestingModule in the imports
-    })
-    .compileComponents();
+      declarations: [MovieComponent],
+      providers: [
+        { provide: ApiService, useValue: apiServiceMock }, // Provide the mock ApiService
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { id: 'exampleId' },
+            },
+          },
+        }, // Mock the ActivatedRoute with a snapshot object
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
